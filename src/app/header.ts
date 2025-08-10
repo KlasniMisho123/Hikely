883189184b1bdc9e11d85f87e45792a37df69bed
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-08-10 21:32:50 +0400

    angular trainingarc

--- src/app/header.ts
+++ src/app/header.ts
@@ -4,6 +4,7 @@ import { Component } from "@angular/core";
     selector: 'app-header',
     standalone: true,
     templateUrl: './header.html',
+    styleUrl: './header.css'
 })
 
 export class HeaderComponent {}

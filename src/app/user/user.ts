759892adfbe65e135fb88fb300982a6357e2f001
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-08-11 19:39:11 +0400

    signals & computed

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,8 +1,5 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, Input,  } from '@angular/core';
 
-import { DUMMY_USERS } from '../dummy-users';
-
-const  randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 
 @Component({
   selector: 'app-user',
@@ -11,11 +8,12 @@ const  randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.css'
 })
 export class User {
-  selectedUser = signal(DUMMY_USERS[randomIndex])
-  imagePath = computed(() => this.selectedUser().avatar)
- 
-  onSelectUser() {
-    const  randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
-    this.selectedUser.set(DUMMY_USERS[randomIndex])
+   @Input() avatar!: String; 
+   @Input() name!: String; 
+
+  get imagePath() {
+    return this.avatar;
   }
+
+  onSelectUser() {}
 }

4137656d598a53a438aac8dd376a49f67100b1cb
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-08-11 23:41:03 +0400

    emmiting id

--- src/app/app.ts
+++ src/app/app.ts
@@ -12,4 +12,8 @@ import { DUMMY_USERS } from './dummy-users';
 export class App {
   protected readonly title = signal('Hikely');
   users = DUMMY_USERS
+
+  onSelectUser(id:string ) {
+    console.log('Selected user ID:', id);
+  }
 }

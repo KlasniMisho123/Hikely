d2a42ad9d34bbce7849a3f97a00c9f8e92094e1b
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-08-13 20:11:08 +0400

    for/if commit

--- src/app/task/task.ts
+++ src/app/task/task.ts
@@ -1,9 +1,9 @@
 import { Component, Input } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
-
+import { TaskComponent } from './task-component/task-component';
 @Component({
   selector: 'app-task',
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './task.html',
   styleUrl: './task.css'
 })

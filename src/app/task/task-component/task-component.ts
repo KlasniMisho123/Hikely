f3bd75c2d22b0c0fd31ffbdfb141767b38d4525e
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-08-15 23:06:03 +0400

    task services

--- src/app/task/task-component/task-component.ts
+++ src/app/task/task-component/task-component.ts
@@ -1,9 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Task } from './task-component.model';
+import { Card } from "../../shared/card/card";
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-task-component',
-  imports: [],
+  imports: [Card, DatePipe],
   templateUrl: './task-component.html',
   styleUrl: './task-component.css'
 })

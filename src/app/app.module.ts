0cda1cdd733f40193d8dd08037708c60d6a97e93
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-08-17 20:13:30 +0400

    ngmodule commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from "@angular/core";
 import { App } from "./app"; 
+import { HeaderComponent } from "./header/header";
+import { User } from "./user/user";
+import { TaskComponent } from "./task/task-component/task-component";
 
 @NgModule({
     declarations: [App],
     bootstrap: [App],
+    imports: [HeaderComponent, User, TaskComponent],
 })
 
 export class AppModule {
